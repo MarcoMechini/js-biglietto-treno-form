1adcda096702664a7d6dd745a413484cc4437f32
Marco Mechini <mechinimarco35@gmail.com>
2024-11-08 16:21:29 +0100

    feat: html: create user card js: create dinamic user date

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const userForm = document.getElementById("user-form");
 const userName = document.getElementById("username");
 const userDistance = document.getElementById("distance");
 const userAge = document.getElementById("age");
+const userCard = document.getElementById("user-card");
+const detName = document.querySelector("#detName span:nth-child(2)");
+const detSc = document.querySelector("#detSc span:nth-child(2)");
+const detTar = document.querySelector("#detTar span:nth-child(2)");
 
 
 
@@ -11,24 +15,26 @@ userForm.addEventListener("submit", function (event) {
     const name = userName.value.trim()
     const distance = parseInt(userDistance.value);
     const age = parseInt(userAge.value);
+    const ticketPrice = calculateTicketPrice(age, distance).toFixed(2);
 
-    console.log(calculateTicketPrice(age, distance))
+    userCard.classList.remove("d-none")
+
+    detName.innerHTML = name;
+    detSc.innerHTML = 'Lo sconto è: ', 'SCONTO DA INSERIRE';
+    detTar.innerHTML = ticketPrice;
 })
 
 
-
-
-
-
-
-
-
-
-
+/**
+ * Description placeholder
+ *
+ * @param {number} eta
+ * @param {number} kilometri
+ * @returns {number}
+ */
 const calculateTicketPrice = (eta, kilometri) => {
     let costoAlKm = 0.21;
     let sconto = 0;
-    let prezzo = 0;
 
     if (eta < 18) {
         sconto = 20;
@@ -36,5 +42,5 @@ const calculateTicketPrice = (eta, kilometri) => {
         sconto = 40;
     }
 
-    return prezzo = kilometri * costoAlKm * (100 - sconto) / 100;
+    return kilometri * costoAlKm * (100 - sconto) / 100;
 }
